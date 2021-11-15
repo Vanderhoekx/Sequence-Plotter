16af03eb0339359421a0cafd3012e1ec42374172
Kelly Schmidt <schmidtrades@hotmail.com>
2021-11-15 12:08:54 -0700

    added colors, markers and annotations

--- sequencesgui.py
+++ sequencesgui.py
@@ -3,19 +3,32 @@ import tkinter as tk
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 from matplotlib.figure import Figure
+import mplcursors
 
 class GraphGui(Sequences):
     def make_graph(self, sequence: list) -> plt.plot:
+        plt.style.use('seaborn-darkgrid')
+        xticks = [num for num in range(len(sequence) + 1)]
+        
         root = tk.Tk()
         root.wm_title('Sequences Plot')
         
         fig = Figure(figsize=(5, 4), dpi=100)
-        fig.add_subplot(111).plot(sequence)
-
+        fig.set_facecolor('#828282')
+        
+        
+        ax = fig.add_subplot(111)
+        ax.tick_params(colors = 'white')
+        ax.set_xticks(xticks)
+        ax.set_xlabel('Cycle', color = 'white')
+        ax.set_ylabel('Value', color = 'white')
+        marker = ax.plot(sequence, color = 'black', marker = 'o', markerfacecolor = 'silver')
+        
         canvas = FigureCanvasTkAgg(fig, master=root)
+        mplcursors.cursor(marker)
+        
         canvas.draw()
         canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-        
         toolbar = NavigationToolbar2Tk(canvas, root)
         toolbar.update()
         
@@ -27,4 +40,4 @@ class GraphGui(Sequences):
 if __name__ == '__main__':
     bailey = GraphGui()
 
-    print(bailey.make_graph(bailey.penta_nums(15)))
+    print(bailey.make_graph(bailey.collatz(15)))
